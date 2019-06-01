663c812ed651844cedc4fe65212547410886deaf
aparnasdec8 <33921526+aparnasdec8@users.noreply.github.com>
2019-06-01 13:44:49 +0530

    Add files via upload

---
+++ index.blade.php
@@ -0,0 +1,468 @@
+<!--Author: W3layouts
+Author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE html>
+<html lang="zxx">
+<head>
+    <title>Dream Home</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="utf-8"/>
+    <meta name="keywords" content="Vicarage Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
+	SmartPhone Compatible web template, free WebDesigns for Nokia, Samsung, LG, Sony Ericsson, Motorola web design" />
+    <script>
+        addEventListener("load", function () {
+            setTimeout(hideURLbar, 0);
+        }, false);
+
+        function hideURLbar() {
+            window.scrollTo(0, 1);
+        }
+    </script>
+    <script>
+    var msg = '{{Session::get('alert')}}';
+    var exist = '{{Session::has('alert')}}';
+    if(exist){
+      alert(msg);
+    }
+  </script>
+  	<style>
+	h3{
+ 
+  font-size: 25pt;         
+  font-style: normal; 
+  font-weight: bold; 
+  color:SlateBlue;
+  
+}
+
+table{
+
+  color:black; 
+  font-size: 11pt; 
+  font-style: normal;
+  font-weight: bold;
+  text-align:; 
+
+  border-collapse: collapse; 
+
+}
+table.inner{
+  border: 0px
+}
+</style>
+    <!-- Custom Theme files -->
+    <link href="css/bootstrap.css" type="text/css" rel="stylesheet" media="all">
+    <link href="css/style.css" type="text/css" rel="stylesheet" media="all">
+    <link rel="stylesheet" type="text/css" href="css/oh-autoval-style.css" media="all">
+    <script src="js/jquery-3.2.1.min.js"> </script>
+    <script src="js/oh-autoval-script.js"></script>
+
+    <!-- font-awesome icons -->
+    <link href="css/fontawesome-all.min.css" rel="stylesheet">
+    <!-- //Custom Theme files -->
+	<link rel="stylesheet" href="css/flexslider.css" type="text/css" media="screen" property="" />
+    <!-- online-fonts -->
+    <link href="//fonts.googleapis.com/css?family=Montserrat:200,300,400,500,600,700,800,900" rel="stylesheet"><!-- //online-fonts -->
+</head>
+<body>
+    <!-- banner -->
+    <div class="banner">
+        <!-- header -->
+        <header>
+            <nav class="navbar navbar-expand-lg navbar-light bg-gradient-secondary">
+                <h1>
+                    <a class="navbar-brand text-white" href="/">
+                        DREAM HOME
+                    </a>
+                </h1>
+                <button class="navbar-toggler ml-md-auto" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav ml-lg-auto text-center">
+                        <li class="nav-item active  mr-3 mt-lg-0 mt-3">
+                            <a class="nav-link" href="/">Home
+                                <span class="sr-only">(current)</span>
+                            </a>
+                        </li>
+                        <li class="nav-item  mr-3 mt-lg-0 mt-3">
+                            <a class="nav-link" href="/services">Services</a>
+                        </li>
+                        <li class="nav-item mr-3 mt-lg-0 mt-3">
+                            <a class="nav-link" href="/about">About</a>
+                        </li>
+                        <li class="nav-item mr-3 mt-lg-0 mt-3">
+                            <a class="nav-link" href="/gallery">Gallery</a>
+                        </li>
+                        
+                        <li>
+                            <button type="button" class="btn  ml-lg-2 w3ls-btn" data-toggle="modal" aria-pressed="false" data-target="#exampleModal">
+                                Login
+                            </button>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </header>
+        <!-- //header -->
+        <div class="container">
+            <!-- banner-text -->
+            <div class="banner-text">
+                <div class="slider-info">
+                    <h3>Find a Home to Suit Your Lifestyle</h3>
+                    <a class="btn btn-primary mt-lg-5 mt-3 agile-link-bnr" href="/about" role="button">View More</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- //container -->
+    <!-- //banner -->
+	<!-- courses -->
+	
+    <!-- //newsletter-->
+
+	<!-- video and events -->
+
+	<!-- //video and events -->
+	<!-- Footer -->
+		<footer id="footer" class="py-5">
+			<div class="container">
+				<div class="row  py-lg-5">
+					<div class="col-lg-3 col-sm-6 footer-logo">
+						<h5>about us</h5>
+						<h2>
+							<a href="index.html">Real Estate</a>
+						</h2>
+						<p class="mt-3">Real estate is "property consisting of land and the buildings on it, along with its natural resources such as crops, minerals or water; immovable property of this nature; an interest vested in this an item of real property, buildings or housing in general.</p>
+					</div>
+					<div class="col-lg-3 col-sm-6 mt-sm-0 mt-4">
+						<h5>Quick links</h5>
+						<ul class="list-unstyled quick-links">
+							<li>
+								<a href="/index">
+								<i class="fa fa-angle-double-right"></i>Home</a>
+							</li>
+							<li>
+								<a href="/about">
+									<i class="fa fa-angle-double-right"></i>About</a>
+							</li>
+							<li>
+								<a href="/services">
+									<i class="fa fa-angle-double-right"></i>Services</a>
+							</li>
+							<li>
+								<a href="/gallery">
+									<i class="fa fa-angle-double-right"></i>Gallery</a>
+							</li>
+							
+						</ul>
+					</div>
+					<div class="col-lg-3 col-sm-6 mt-lg-0 mt-4">
+						<h5>working hours</h5>
+						<ul class="list-unstyled quick-links">
+							<li>
+								Monday - Friday
+								<br>
+								<span>9.00 - 20.00</span>
+							</li>
+							<li>
+								Saturday
+								<br>
+								<span> 10.00 - 16.00 </span>
+							</li>
+							<li>
+								Sunday
+								<br>
+								<span> 9.30 - 18.00 </span>
+							</li>
+						</ul>
+					</div>
+					<div class="col-lg-3 col-sm-6 footer_grid1 mt-lg-0 mt-4">
+						<h5>Address</h5>
+						<div class="fv3-contact">
+							<span class="fas fa-envelope-open mr-2"></span>
+							<p>
+								<a href="mailto:example@email.com">anishathomasmulamgothara@gmail.com</a>
+							</p>
+						</div>
+						<div class="fv3-contact my-2">
+							<span class="fas fa-phone-volume mr-2"></span>
+							<p>+91 9497626357</p>
+						</div>
+						<div class="fv3-contact">
+							<span class="fas fa-home mr-2"></span>
+							<p>+Amal Jyothi College of Engineering,
+								<br>Koovapally.</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</footer>
+		
+	</div>
+	<!-- /Footer -->
+
+	<!-- login  -->
+    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Login</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <form action="{{route('login')}}" method="post">
+                    {{ csrf_field() }}
+                        <div class="form-group">
+                            <label for="recipient-name" class="col-form-label">Username</label>
+							<div class="right-w3l">
+                            <div class="form-group">
+                                <input id="email" name="email" class="form-control">
+
+                                @if ($errors->has('email'))
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('email') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+							</div>
+					    </div>
+                        <div class="form-group">
+                            <label for="password" class="col-form-label">Password</label>
+                            <div class="form-group">
+                                <input type="password" autocomplete="off" class="form-control" name="password" <?php if(isset($_COOKIE['name'])){echo "value='{$_COOKIE['name']}'";}?>>
+                                @if ($errors->has('password'))
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('password') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+                        <div class="reg-w3l">
+						  
+						   <button type="submit" class="form-control submit mb-4">
+									{{ __('Login') }}
+							
+                                </button>
+                        </div>
+                        <div class="form-group row">
+                            <div class="col-md-6 offset-md-4">
+                                <div class="form-check">
+                                   
+                                </div>
+                            </div>
+                        </div>
+                        
+                        </div>
+                        <p class="text-center dont-do">Don't have an account?
+                            <a href="#" data-toggle="modal" data-target="#exampleModalCenter2" class="text-dark font-weight-bold">
+                                Register Now</a>
+								
+                        </p>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- //login -->
+	 <!--/Register-->
+    <div class="modal fade" id="exampleModalCenter2" tabindex="-1" role="dialog" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+                <div class="modal-header text-center">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <div class="login px-4 mx-auto mw-100">
+                        <h5 class="modal-title text-center text-dark mb-4">REGISTER NOW</h5>
+						<form action="{{route('register')}}" method="post" class="oh-autoval-form" onsubmit="return">
+                            @csrf
+                            <table align="center" cellpadding = "10">
+						<input type="hidden" name="_token" value="{{ csrf_token()}}">
+						<tr><td><div class="form-group">
+                                <label class="col-form-label">User Type</label>&nbsp;&nbsp;&nbsp;
+                                <td> <select  name="type" width="200px"   >&nbsp;&nbsp;&nbsp;
+                                <option value="1">Customer</option>
+                                <option value="2">Builder</option>
+                                <option value="3">Engineer</option>
+                            </select></td>
+                            </div></td></tr>
+                           <tr><td> <div class="form-group">
+                                <label class="col-form-label">First name</label>
+
+                                <div class="form-group">
+                                <td> <input id="fname" type="text" name="fname" class="av-name" av-message="Invalid Name">
+                                </td>
+                                @if ($errors->has('fname'))
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('fname') }}</strong>
+                                    </span>
+                                @endif
+                                </div>
+                            </div></td></tr>
+                           <tr><td> <div class="form-group" id="lastname">
+                                <label class="col-form-label">Last name</label>&nbsp;&nbsp;&nbsp;
+                              <td>  <input id="sname" type="text" class="av-name" name="sname" av-message="Invalid Name">
+</td>
+                                @if ($errors->has('sname'))
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('sname') }}</strong>
+                                    </span>
+                                @endif
+							</div>
+							</td></tr>
+                          <tr><td>  <div class="form-group">
+                                <label class="col-form-label">Mobile Number</label>
+                               <td> <input type="text" class="av-mobile" av-message="Invalid mobile address" name="mob">
+                            </td>
+                            </div>
+                           </td></tr>
+                           <tr><td> <div class="form-group">
+                                <label class="col-form-label">Email</label>
+                                <div class="form-group">
+                                <td><input id="emails" name="email"class="av-email" av-message="Invalid email address" required>
+                                </td></div>
+                                @if ($errors->has('email'))
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('email') }}</strong>
+                                    </span>
+                                @endif
+                            
+                            </div></td></tr>
+                           <tr><td> <div class="form-group">
+                                <label class="mb-2 col-form-label">Password</label>
+                               <td> <input type="password" type="password" class="av-password" name="password" av-message="Password must contain uppercase,lowercase,special chars,digits and minimum 6 chars">
+                            </td></div></td></tr>
+                           <tr><td> <div class="form-group">
+                                <label class="col-form-label">Confirm Password</label>
+                             <td>   <input type="password" type="password" class="av-password"  name="conpassword" av-message="Password must contain uppercase,lowercase,special chars,digits and minimum 6 chars">
+                            </td></div></td></tr>
+						<td >	<div class="reg-w3l">
+								<button type="submit" class="form-control submit mb-4">Register</button>
+                           
+                           </div>
+						   </td>
+                        </form>
+</table>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+    <!--//Register-->
+
+    <!-- //footer -->
+    <!-- js -->
+    <script src="js/jquery-2.2.3.min.js"></script>
+    <!-- //js -->
+	<!-- FlexSlider-JavaScript -->
+	<script defer src="js/jquery.flexslider.js"></script>
+	<script type="text/javascript">
+		
+				$(window).load(function(){
+				$('.flexslider').flexslider({
+					animation: "slide",
+					start: function(slider){
+						$('body').removeClass('loading');
+					}
+			});
+		});
+	</script>
+<!-- //FlexSlider-JavaScript -->
+    <!-- script for password match -->
+    <script>
+        window.onload = function () {
+            document.getElementById("password1").onchange = validatePassword;
+            document.getElementById("password2").onchange = validatePassword;
+        }
+
+        function validatePassword() {
+            var pass2 = document.getElementById("password2").value;
+            var pass1 = document.getElementById("password1").value;
+            if (pass1 != pass2)
+                document.getElementById("password2").setCustomValidity("Passwords Don't Match");
+            else
+                document.getElementById("password2").setCustomValidity('');
+            //empty string means no validation error
+		}
+    </script>
+    <!-- script for password match -->
+	<!-- start-smooth-scrolling -->
+	<script>
+        function showtextbox(){
+          var x = document.getElementById("builder");
+          x.style.display = "none";
+          var y= document.getElementById("customer");
+          y.style.display = "none";
+          var z= document.getElementById("engineer");
+	        z.style.display = "none";
+            var a=document.getElementById('company').value;
+            if(a==2)
+            {
+              var x = document.getElementById("builder");
+		          x.style.display = "block";
+            }
+            if(a==3)
+            {
+              var y = document.getElementById("customer");
+		          y.style.display = "block";
+            }
+            if(a==4)
+            {
+              var z = document.getElementById("engineer");
+		          z.style.display = "block";
+            }
+        }
+        </script>
+    <script src="js/move-top.js"></script>
+    <script src="js/easing.js"></script>
+    <script>
+        jQuery(document).ready(function ($) {
+            $(".scroll").click(function (event) {
+                event.preventDefault();
+
+                $('html,body').animate({
+                    scrollTop: $(this.hash).offset().top
+                }, 1000);
+            });
+        });
+    </script>
+    <!-- //end-smooth-scrolling -->
+    <!-- smooth-scrolling-of-move-up -->
+    <script>
+        $(document).ready(function () {
+            /*
+            var defaults = {
+                containerID: 'toTop', // fading element id
+                containerHoverID: 'toTopHover', // fading element hover id
+                scrollSpeed: 1200,
+                easingType: 'linear' 
+            };
+            */
+
+            $().UItoTop({
+                easingType: 'easeOutQuart'
+            });
+
+        });
+    </script>
+    <script src="js/SmoothScroll.min.js"></script>
+    <!-- //smooth-scrolling-of-move-up -->
+    <!-- Bootstrap core JavaScript
+================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+	<script src="js/bootstrap.js"></script>
+    
+    <main class="py-4">
+            @yield('content')
+        </main>
+</body>
+</html>
